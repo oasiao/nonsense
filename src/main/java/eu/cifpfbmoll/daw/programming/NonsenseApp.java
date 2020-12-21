48694b2c2e791e3f759c956c340ca8e56666e3e5
Onika Kim Asiao Dumbrique <oasiao@cifpfbmoll.eu>
2020-12-21 20:37:21 +0100

    feat: console input test

--- src/main/java/eu/cifpfbmoll/daw/programming/NonsenseApp.java
+++ src/main/java/eu/cifpfbmoll/daw/programming/NonsenseApp.java
@@ -1,13 +1,22 @@
 package eu.cifpfbmoll.daw.programming;
 
+import org.apache.commons.lang3.StringUtils;
 import java.util.Scanner;
 
 public class NonsenseApp {
+    private static final String EXIT_KEY="9";
     public static void main(String[] args) {
+        boolean aux=StringUtils.equals("a","A");
+        System.out.println("hola mundo:"+aux);
         //Using scanner for getting input from user
         Scanner in=new Scanner(System.in);
-        String s=in.nextLine();
-        System.out.println("You entered string"+s);
+        boolean isExit=false;
+        while(!isExit){
+            String s=in.nextLine();
+            System.out.println("You entered string"+s);
+            isExit=StringUtils.equals(s,EXIT_KEY);
+        }
+
         System.out.println("-END-");
     }
 }
